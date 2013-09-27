d0a5ee0603178541324f194a28319c13fc087ec0
Kjell Hedstrom <Hedstrom@KjellKod.cc>
2013-09-27 00:20:00 -0600

    iIn another threadwq

--- TcmallocExample/src/main.cpp
+++ TcmallocExample/src/main.cpp
@@ -2,11 +2,12 @@
 
 #include <string>
 #include <iostream>
+#include <thread>
 #include <vector>
 #include "Allocate.h"
 #include <gperftools/malloc_extension.h>
 
-int main() {  
+void mainX() {  
 //http://gperftools.googlecode.com/svn/trunk/src/tests/frag_unittest.cc
   
   size_t heap_before;
@@ -22,4 +23,11 @@ int main() {
   std::vector<char> buffer(2000, '0');
   MallocExtension::instance()->GetStats(&buffer[0], 2000);
   std::cout << &buffer[0] << std::endl;
+}
+
+
+int main() {
+   std::thread t1(mainX);
+   t1.join();
+   return 0;
 }
